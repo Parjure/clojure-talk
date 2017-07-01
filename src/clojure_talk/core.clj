4d450c9b71cc3d295326f8d626f7cd3a0abd6089
Gilles Philippart <gilles.philippart@gmail.com>
2017-07-01 12:34:25 +0200

    Initial version

---
+++ src/clojure_talk/core.clj
@@ -0,0 +1,42 @@
+(ns clojure-talk.core)
+
+
+;;; basic data structures
+
+
+;; strings
+"Clojure rocks"
+
+(class "Clojure rocks")
+
+;; numbers
+42
+0.01
+
+;; symbols
+'dwayne
+
+;; keywords
+:age
+
+;; precise arithmetic
+(/ 1 3)
+
+
+;; collections
+{:firstname "Ellen" :lastname "Ripley"}
+
+#{"a" "set" "of" "words"}
+[23 42 99]
+
+
+
+
+;; nested collections
+{:firstname "Ellen" :lastname "Ripley" :guns [ {:type :flamethrower
+                                                :model "]
+
+
+;; regex
+
+
